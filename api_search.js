a716d145af44f9bd443a8e4278de0b1f634c1bfc
ItsJiran <its.gilangramadhan@gmail.com>
2024-07-01 09:01:04 +0700

    add

--- api_search.js
+++ api_search.js
@@ -1,14 +1,43 @@
 
 class BlogPostEntity{
-  
-  constructor(entry){
-    this.author = entry.author;  
-    this.content      = entry.content.$t;
-    this.contnet_type = entry.content.type;
-  }
-
-  
-
+	/*
+	author    : [{…}]
+	content   : {type: 'html', $t: '<p>&nbsp;<span style="background-color: white; fon…, ut interdum velit lobortis eu.</span>&nbsp;</p>'}
+	id        : {$t: 'tag:blogger.com,1999:blog-5950330201730594593.post-5916909838795192142'}
+	link      : (5) [{…}, {…}, {…}, {…}, {…}]
+	published : {$t: '2024-06-27T21:32:00.002-07:00'}
+	thr$total : {$t: '0'} 
+	title     : {type: 'text', $t: 'Testing Artikel 231456'}
+	updated   : {$t: '2024-06-27T21:32:54.257-07:00'}
+	*/
+	constructor(entry){
+		this.authors      = this.buildAuthors(entry.author);  
+		this.content      = entry.content.$t;
+		this.content_type = entry.content.type;
+		this.published    = entry.published.$t;
+		this.updated 	  = entry.updated.$t;	
+		this.links        = entry.link;
+	}
+	buildAuthors(authors){
+		let cont = [];
+		for(let author of authors)
+			cont.push( new BlogAuthorEntity(author) );		
+		return cont;
+	}
+}
+class BlogAuthorEntity{
+	/*
+	email    : {$t: 'noreply@blogger.com'}
+	gd$image : {rel: 'http://schemas.google.com/g/2005#thumbnail', width: '32', height: '17', src: '//blogger.googleusercontent.com/img/b/R29vZ2xl/AVv…u8TH_TkhcmSEWC5uR73LQZbpVCazJ/s220/cs-snedel.webp'}
+	name     : {$t: 'Gilang Ramadhan'}
+	uri      : {$t: 'http://www.blogger.com/profile/07000369991075541029'}
+	*/
+	constructor(author_entry){
+		this.email = author_entry.email.$t;
+		this.image = author_entry.gd$image;
+		this.name  = author_entry.name.$t;
+		this.uri   = author_entry.uri.$t;
+	}
 }
 
 
@@ -16,15 +45,14 @@ class BlogSearch{
 	constructor(){
 		this.BlogSearchInfo = new BlogSearchInfo();
 		this.BlogSearchApi  = new BlogSearchApi( this.BlogSearchInfo );
+		this.BlogPosts      = [];
 	}
 	async run(){
-    // call response json
+		// call response json
 		let response = await this.BlogSearchApi.call();
 
-    // get the data for pagination and posts
-	}
-	apiUrl(){
-		return this.BlogSearchApi.url;
+		// get the data for pagination and posts
+		console.log(  )
 	}
 }
 class BlogSearchInfo{
@@ -83,9 +111,9 @@ class BlogSearchInfo{
 class BlogSearchApi{
 	constructor(blog_info){
 		this.url      = '';
-    this.queries  = '';
-    this.path     = '';
-		this.target       = '/feeds/posts/default';
+		this.queries  = '';
+		this.path     = '';
+		this.target   = '/feeds/posts/default';
 		this.property = {      
 			'start-index' : 1,
 			'max-result' : 6,
@@ -114,8 +142,8 @@ class BlogSearchApi{
 
 		// api queries
 		this.queries = this.buildQueries(blog_info);
-    this.path    = '';
-      
+		this.path    = '';
+
 		// determine if there's a label search or not
 		if(blog_info.label != undefined) this.path = '/-/' + blog_info.label;
 
@@ -126,7 +154,7 @@ class BlogSearchApi{
 		// setup pagination propety
 		let queries = this.property;
 		queries['start-index'] += ( this.property["max-result"] * ( blog_info.queries['current-page'] - 1 ) );
-      
+
 		for( let key in queries ){
 			if( path.length != 1 ) path += '&';
 			if( Array.isArray(queries[key]) ) {
